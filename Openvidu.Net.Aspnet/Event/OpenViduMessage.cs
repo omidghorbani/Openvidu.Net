b2d9aa3307ccae6b2737734b8df79941a597c1fb
Omid <omid.ghorbani@live.com>
2022-10-23 22:44:57 +0330

    add message event

---
+++ Openvidu.Net.Aspnet/Event/OpenViduMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace Openvidu.Net.Aspnet.Event;
+
+public class OpenViduMessage
+{
+    public string Message { get; set; }
+    public string SessionId { get; set; }
+    public string[] Members { get; set; }
+}
