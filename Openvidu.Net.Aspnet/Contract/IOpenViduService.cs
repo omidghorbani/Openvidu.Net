f0d9d24f347e3d7ca353f63e9bcdeb55bdc7214b
Omid <omid.ghorbani@live.com>
2022-10-23 22:45:43 +0330

    update main service

--- Openvidu.Net.Aspnet/Contract/IOpenViduService.cs
+++ Openvidu.Net.Aspnet/Contract/IOpenViduService.cs
@@ -5,5 +5,5 @@ namespace Openvidu.Net.Aspnet.Contract;
 public interface IOpenViduService
 {
     void StoreEvent(OpenViduEvent @event);
-    void RegisterHandler(Func<OpenViduEvent, Task> func);
+    void RegisterHandler(Func<OpenViduEvent, Task>? func);
 }
--- Openvidu.Net.Aspnet/Services/OpenViduService.cs
+++ Openvidu.Net.Aspnet/Services/OpenViduService.cs
@@ -1,54 +1,60 @@
 ﻿using System.Threading.Channels;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Openvidu.Net.Aspnet.Contract;
 using Openvidu.Net.Aspnet.Event;
 
 namespace Openvidu.Net.Aspnet.Services;
 
-public class OpenViduService : IOpenViduService
+public class OpenViduService : IOpenViduService, IHostedService
 {
-    private Channel<OpenViduEvent> jobs = null;
+    private readonly Channel<OpenViduEvent> _jobs;
+    private readonly ILogger<OpenViduService> _logger;
 
-    private Func<OpenViduEvent, Task> _func = null;
-
-    public OpenViduService(IHostApplicationLifetime applicationLifetime)
+    private Func<OpenViduEvent, Task>? _func;
+    
+    public OpenViduService(IHostApplicationLifetime applicationLifetime, ILogger<OpenViduService> logger)
     {
-        jobs = Channel.CreateUnbounded<OpenViduEvent>(new UnboundedChannelOptions
+        _logger = logger;
+        _jobs = Channel.CreateUnbounded<OpenViduEvent>(new UnboundedChannelOptions
         {
             SingleReader = true,
             SingleWriter = false
         });
 
-        applicationLifetime.ApplicationStopping.Register(() => jobs?.Writer.TryComplete());
+        applicationLifetime.ApplicationStopping.Register(() => _jobs?.Writer.TryComplete());
         applicationLifetime.ApplicationStarted.Register(async () => await ProcessQueue());
     }
 
-    public void RegisterHandler(Func<OpenViduEvent, Task> func)
+    public void RegisterHandler(Func<OpenViduEvent, Task>? func)
     {
         _func = func;
     }
 
     public void StoreEvent(OpenViduEvent @event)
     {
-        jobs.Writer.TryWrite(@event);
+        _jobs.Writer.TryWrite(@event);
     }
 
     private async Task ProcessQueue()
     {
-        while (!jobs.Reader.Completion.IsCompleted)
+        while (!_jobs.Reader.Completion.IsCompleted)
         {
             try
             {
-                if (!await jobs.Reader.WaitToReadAsync())
+                if (!await _jobs.Reader.WaitToReadAsync())
                     break;
                 if (_func == null)
                     break;
 
                 OpenViduEvent? item;
-                while (jobs.Reader.TryRead(out item))
+                while (_jobs.Reader.TryRead(out item))
                 {
                     if (_func != null)
+                    {
                         await _func(item);
+
+                    }
                 }
 
             }
@@ -64,4 +70,24 @@ public class OpenViduService : IOpenViduService
         }
     }
 
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        
+        _logger.LogInformation($"The {nameof(OpenViduService)} started.");
+
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        
+        _logger.LogInformation($"The {nameof(OpenViduService)} stopping.");
+
+        _jobs.Writer.TryComplete();
+
+        _logger.LogInformation($"The {nameof(OpenViduService)} stopped.");
+
+        return Task.CompletedTask;
+
+    }
 }
