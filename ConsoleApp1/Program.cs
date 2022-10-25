3788264fe6c8e12ab7216d41f8257b3f5d854010
Omid <omid.ghorbani@live.com>
2022-10-25 11:20:18 +0330

    update vr config

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,12 +7,12 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            var ov = new OpenVidu("https://medias.shooka.com:443", 
-                "grkWXM6aYwQsPrikkW2EPl6pBgx3yszHWz0W842JTYvT38LNJbBYOwUH5McH6xhF");
+            var ov = new OpenViduManager("https://vr.atreya.co", 
+                "MY_SECRET");
             var session = ov.CreateSession();
 
-            ov.Fetch();
-            ov.StartRecording(session.Id, "11");
+            ov.FetchSessions();
+            ov.StartRecordingSession(session.Id, "11");
             var lsi = ov.GetActiveSessions();
 
             Console.WriteLine("Hello, World!");
