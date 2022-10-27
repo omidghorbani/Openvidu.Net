597b59a12a7aaa29d4f6aa02b915784546bf5794
Omid <omid.ghorbani@live.com>
2022-10-27 16:53:42 +0330

    add Media Node Dtos for using on API

---
+++ Openvidu.Net/DTOs/MediaNodeAws.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Openvidu.Net.DTOs;
+
+public class MediaNodeAws
+{
+
+    /// <summary>
+    /// instanceType (optional String only for AWS deployments) : a valid EC2 instance type. If specified, a Media Node will be created using the specified EC2 instance type. If not especified, value AWS_INSTANCE_TYPE in /opt/openvidu/.env will be used. This property is only taken into account in AWS deployments.
+    /// </summary>
+    [JsonPropertyName("instanceType")]
+    public string InstanceType { get; set; }
+
+    /// <summary>
+    /// volumeSize (optional Number only for AWS deployments) : Volume size for the new Media Node in GB. If specified, the Media Node will be created with such disk volume size. If not especified, value AWS_VOLUME_SIZE in /opt/openvidu/.env will be used. This property is only taken into account in AWS deployments.
+    /// </summary>
+    [JsonPropertyName("volumeSize")]
+    public int VolumeSize { get; set; }
+}
---
+++ Openvidu.Net/DTOs/MediaNodeOnPremises.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Openvidu.Net.DTOs;
+
+public class MediaNodeOnPremises
+{
+    /// <summary>
+    /// uri (mandatory String only for On Premises deployments) : the websocket endpoint of a running Media Node. Should be something similar to ws://media.server.ip:8888/kurento. This property is only necessary and is only taken into account On Premises deployments. For other deployment environments a new Media Node will be automatically launched completely ignoring parameter uri.
+    /// </summary>
+    [JsonPropertyName("uri")]
+    public string Uri { get; set; }
+
+    /// <summary>
+    /// environmentId (optional String only for On Premises deployments) : a custom environment id. This can help further identify your on premises Media Node.
+    /// </summary>
+    [JsonPropertyName("environmentId")]
+    public int EnvironmentId { get; set; }
+}
